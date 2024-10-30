6c0bcbe886fdf57138ad4a6075c183c52f5fbc26
tarekk2 <tarekk2@users.noreply.github.com>
2024-10-29 23:24:15 -0300

    Clean up logs

--- Source/MemoryPatterns/PatternTypes/SceneObjectUpdaterPatternTypes.h
+++ Source/MemoryPatterns/PatternTypes/SceneObjectUpdaterPatternTypes.h
@@ -10,3 +10,4 @@ template <typename FieldType, typename OffsetType>
 using SceneObjectUpdaterOffset = FieldOffset<cs2::SceneObjectUpdaterHandle_t, FieldType, OffsetType>;
 
 STRONG_TYPE_ALIAS(OffsetToSceneObject, SceneObjectUpdaterOffset<cs2::SceneObjectUpdaterHandle_t::sceneObject, std::int8_t>);
+
