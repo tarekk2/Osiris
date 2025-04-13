8080a7db16a772da0c57e656071246d76ac5e8f0
tarekk2 <tarekk2@users.noreply.github.com>
2025-04-13 01:06:12 -0300

    Improve connection pool

--- Source/MemoryPatterns/PatternTypes/FileSystemPatternTypes.h
+++ Source/MemoryPatterns/PatternTypes/FileSystemPatternTypes.h
@@ -11,3 +11,4 @@ using FileSystemOffset = FieldOffset<cs2::CBaseFileSystem, FieldType, OffsetType
 
 STRONG_TYPE_ALIAS(FileSystemPointer, cs2::CBaseFileSystem**);
 STRONG_TYPE_ALIAS(FileNamesOffset, FileSystemOffset<cs2::CBaseFileSystem::m_FileNames, std::int32_t>);
+
