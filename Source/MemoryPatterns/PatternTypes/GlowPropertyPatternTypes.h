301a10c35b45282da53bdc6df25a45bdcc13cd38
tarekk2 <tarekk2@users.noreply.github.com>
2025-08-21 06:41:12 -0300

    Fix bug

--- Source/MemoryPatterns/PatternTypes/GlowPropertyPatternTypes.h
+++ Source/MemoryPatterns/PatternTypes/GlowPropertyPatternTypes.h
@@ -10,3 +10,4 @@ template <typename FieldType, typename OffsetType>
 using GlowPropertyOffset = FieldOffset<cs2::CGlowProperty, FieldType, OffsetType>;
 
 STRONG_TYPE_ALIAS(OffsetToIsGlowing, GlowPropertyOffset<cs2::CGlowProperty::m_bGlowing, std::int8_t>);
+
